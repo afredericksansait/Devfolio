f18e1775326bebfd3283b2c9af95c6c756d43693
A. Frederick S <afsansait@gmail.com>
2020-11-16 22:12:11 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -118,7 +118,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: `ADD YOUR TRACKING ID HERE`, // Optional Google Analytics
+        trackingId: `G-Z6NZZFKBH4`, // Optional Google Analytics
       },
     },
     {
